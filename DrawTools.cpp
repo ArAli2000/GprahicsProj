e3fc1130e65f1d09ed81350bc698f9c555cafef7
KhaledNowara <kn_nowara@outlook.com>
2022-11-18 00:07:53 +0100

    Changes: - Added base types with vector - Changed player movement - Added ball - Extracted drawCircle to DrawTools.cpp

---
+++ DrawTools.cpp
@@ -0,0 +1,10 @@
+#include <GL/glut.h>
+
+void drawCircle(int x, int y, float r)
+	{
+		glPushMatrix();
+		glTranslatef(x, y, 0);
+		GLUquadric *quadObj = gluNewQuadric();
+		gluDisk(quadObj, 0, r, 1000, 1000);
+		glPopMatrix();
+	}
